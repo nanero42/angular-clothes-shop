1aff3efde72114ed9a96f2753a5479e50411ffb4
Nariman <n.murzakaev@big3.ru>
2023-06-01 22:19:15 +0300

    show/hide navbar om pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'e-commerce';
+  showNavbar = true;
+  pages = ['/'];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.hideShowNavbar$().subscribe();
+  }
+
+  hideShowNavbar$() {
+    return this.router.events.pipe(
+      tap((route) => {
+        if (route instanceof NavigationEnd) {
+          this.pages.some((url) => url === route.url)
+            ? this.showNavbar = false
+            : this.showNavbar = true;
+        }
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from './modules/navbar/navbar.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { NavbarModule } from './modules/navbar/navbar.module';
     BrowserModule,
     AppRoutingModule,
     NavbarModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
